2ad2158196b42ebaf20e7bcf05690c126ba31a8a
aavolkova <avolkova@montgomerycollege.edu>
2017-08-02 23:56:53 -0400

    First commit

---
+++ src/datedifference/DateDifference.java
@@ -0,0 +1,56 @@
+package datedifference;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Scanner;
+
+public class DateDifference {
+	/**
+	 * This application tell the difference between two dates in days.
+	 * @author Anna Volkova
+	 */	
+	public static void main(String[] args) {
+		
+	 	int year1 = 2012;
+	 	int year2 = 2017;
+		
+	 	int month1 = 2;
+	 	int month2 = 2;
+		
+	 	int dayOfMonth1 = 29;
+	 	int dayOfMonth2 = 28;
+	 
+	 	// Create a Scanner object
+	 	Scanner input = new Scanner(System.in);
+	 	
+	 	System.out.println("*** Calculate Duration Between Two Dates ***");
+	 	System.out.println();
+	 	
+	 	System.out.println("Enter First date: "); 	
+	 	System.out.print("Year YYYY: ");
+	 	year1 = Integer.parseInt(input.next());
+	 	System.out.print("Month MM: ");
+	 	month1 = Integer.parseInt(input.next());
+	 	System.out.print("Day DD: ");
+	 	dayOfMonth1 = Integer.parseInt(input.next());
+	 	System.out.println();
+	 	System.out.println("Enter Second date: ");
+	 	System.out.print("Year YYYY: ");
+	 	year2 = Integer.parseInt(input.next());
+	 	System.out.print("Month MM: ");
+	 	month2 = Integer.parseInt(input.next());
+	 	System.out.print("Day DD: ");
+	 	dayOfMonth2 = Integer.parseInt(input.next());
+	 	System.out.println();	 	
+	 	
+	 	// Create two dates
+	 	LocalDate firstDate = LocalDate.of(year1, month1, dayOfMonth1 );
+	 	LocalDate secondDate = LocalDate.of(year2, month2, dayOfMonth2);
+		
+	 	// Calculate difference in days
+		final long daysBetween = ChronoUnit.DAYS.between(firstDate, secondDate);
+		
+		// Display result
+        System.out.println("Days between two dates: " + daysBetween);		
+	}
+}
